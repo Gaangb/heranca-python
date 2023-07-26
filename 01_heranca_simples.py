d02082070b5c247745fbd21945d8888c29492706
Gaangb <angelob4stos@gmail.com>
2023-07-26 12:07:25 -0300

    feat: Finalizando classe caminhao

--- 01_heranca_simples.py
+++ 01_heranca_simples.py
@@ -1,3 +1,8 @@
+# Neste código faço praticas de POO usando a lingaguem python. 
+# Estou criando uma classe Veiculo, Motocicleta, Carro e Caminhao, sendo que as ultimas 3 herdam de Veiculo
+# Adicionei em Caminhao opcao de carga, descarga e verificar carga
+
+
 class Veiculo:
     def __init__(self, cor,placa,numero_rodas):
         self.cor = cor
@@ -6,6 +11,9 @@ class Veiculo:
 
     def ligar_motor(self):
         print("Motor ligado")
+
+    def __str__(self):
+        return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
 
 class Motocicleta(Veiculo):
     pass
@@ -26,16 +34,15 @@ class Caminhao(Veiculo):
             print("Caminhão já esta carregado")
         else:
             self.carregado = True
+            print("Carga adicionada")
 
     def descarregar(self):
         if self.carregado:
             self.carregado = False
+            print("Carga retirada")
         else:
             print("Caminhão não possui carga")
     
-
-    def __str__(self):
-        return self.cor
 
 
 
@@ -49,4 +56,7 @@ caminhao = Caminhao("Branco", "JKJ-2211", 8)
 caminhao.verificar_carga()
 caminhao.adicionar_carga()
 caminhao.verificar_carga()
-caminhao.ligar_motor()
+
+print(caminhao)
+print(moto)
+print(carro)
